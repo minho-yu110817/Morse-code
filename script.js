0284095ac5e4b109f920e6a9de2e8f66ad73c485
minho-yu110817 <meyh110817@naver.com>
2025-04-04 21:56:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-const ddu = document.getElementById('ddu')
-const dduu = document.getElementById('dduu')
-const ddubtn = document.getElementById('ddubtn')
-const dduubtn = document.getElementById('dduubtn')
+const ddu = document.getElementById('ddu');
+const dduu = document.getElementById('dduu');
+const ddubtn = document.getElementById('ddubtn');
+const dduubtn = document.getElementById('dduubtn');
+const morse = document.getElementById('morse');
+const resetbtn = document.getElementById('resetbtn');
 
-ddubtn.addEventListener('click', function() {
-  ddu.currentTime = 0;
-  ddu.play();
-})
+const morseToChar = {
+  '-----': '0',
+  '·----': '1',
+  '··---': '2',
+  '···--': '3',
+  '····-': '4',
+  '·····': '5',
+  '-····': '6',
+  '--···': '7',
+  '---··': '8',
+  '----·': '9',
+};
 
-dduubtn.addEventListener('click', function() {
-  dduu.currentTime = 0;
-  dduu.play();
-})
+let currentMorse = '';
+
+function handleInput(symbol, sound) {
+  currentMorse += symbol;
+  morse.innerHTML += symbol;
+
+  if (morseToChar[currentMorse]) {
+    const char = morseToChar[currentMorse];
+    morse.innerHTML = morse.innerHTML.slice(0, -currentMorse.length) + char;
+    currentMorse = '';
+  }
+
+  sound.currentTime = 0;
+  sound.play();
+}
+
+ddubtn.addEventListener('click', () => handleInput('·', ddu));
+dduubtn.addEventListener('click', () => handleInput('-', dduu));
+
+resetbtn.addEventListener('click', function () {
+  morse.innerHTML = '';
+  currentMorse = '';
+});
